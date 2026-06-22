1dc6723e5c28ae45feb407cd1194df4cd7810f9e
AMB-007 <arjunmbabu777@gmail.com>
2026-06-22 10:04:04 +0530

    updated code

--- Decision Making/age_multiple_conditions.py
+++ Decision Making/age_multiple_conditions.py
@@ -2,9 +2,9 @@ age = int(input("Enter the age:"))
 
 if age >= 18:
     print(f"He can vote")
-elif age >= 17:
+elif age == 17:
     print(f"He can learn driving")
-elif age >= 16 :
+elif age == 16 :
     print(f"He can buy lottery")
 else:
     print(f"He can go to Trick treating")
