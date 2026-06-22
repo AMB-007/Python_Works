8816dc1183240b5a2ddf254cb548db9e8e327baf
AMB-007 <arjunmbabu777@gmail.com>
2026-06-22 20:02:52 +0530

    code updated

--- Decision Making/atm_machine.py
+++ Decision Making/atm_machine.py
@@ -1,40 +1,28 @@
 balance = 10000
 min_balance = 1000
 
-
 print(f"1)check balance\n2)Deposit Money\n3)Withdraw Amount\n4)Exit")
 choice = input("Enter the choice :")
 
 if choice == "1":
-    print(f"Yout avaliable balance is : {balance}")
+    print(f"The balance is :{balance}")
+
 elif choice == "2":
     deposit_amount = int(input("Enter the deposit amount :"))
     balance += deposit_amount
-    print(f"Your avaliable balance is :{balance}")
+    print(f"The deposied amount is : {deposit_amount}and the updated balance is :{balance}")
 
-
-elif choice == "3":
-    withdraw_amount = int(input("Enter the withdrawal amount : "))
-
-    if balance >= withdraw_amount:
-        print(f"You can withdraw amount{withdraw_amount}")
+elif choice =="3":
+    withdraw = int(input("Enter the withdrawal amount :"))
+    if withdraw > balance:
+        print(f"Insufficient Balance")
+    elif balance - withdraw < min_balance:
+        print(f"Minimum Balance of{min_balance} must be maintained Your balance is {balance}")
+    else:
+        balance -= withdraw
+        print(f"The withdrawal successful\nAvaliable balance is{balance}")
         
-    elif balance == withdraw_amount:
-        print(f"in sufficient balance{withdraw_amount}")
-        if balance >= min_balance:
-
-       
-            balance - withdraw_amount
-            print(f"The balance amount is {balance}")    
-        
-
-    elif min_balance >= balance:
-        print(f"Cant withdraw the money minmum balance 1000 is needed{min_balance}")
-
-
-
 elif choice == "4":
-    print(f"You can exit")
-       
+    print(f"THANK YOU FOR USING THE ATM")
 else:
-    print(f"can't do any thing Sorry")
+    print(f"Invalid Choice Please choose Correct Option")
