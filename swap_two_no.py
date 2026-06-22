a29a2230c467c9abfae985bc40b3836d7b8e80ff
AMB-007 <arjunmbabu777@gmail.com>
2026-06-22 09:29:00 +0530

    Added new Python programs

--- swap_two_no.py
+++ swap_two_no.py
@@ -1,12 +1,19 @@
-
-
-
+# method one
 num_1 = 10
 num_2 = 20
-num_1 , num_2 = num_2 , num_1
+#num_1 , num_2 = num_2 , num_1 
 
-temp = num_2
-num_2 = num_2
-num_2 = num_1
+#method two
+#temp = num_2
+#num_2 = num_2
+#num_2 = num_1
+
+
+#using arithematic operators
+num_1 = num_1 + num_2 #30
+num_2 = num_1 - num_2 #10
+num_1 = num_1 - num_2 #30 - 10
+
+
 
 print(f"After Swapping: num1 = {num_1}, num2 = {num_2}")
