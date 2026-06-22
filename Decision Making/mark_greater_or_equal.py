71093bc295b8eee8683afc4607edc13e7bcc8340
AMB-007 <arjunmbabu777@gmail.com>
2026-06-22 09:42:35 +0530

    Added new Python programs

--- Decision Making/mark_greater_or_equal.py
+++ Decision Making/mark_greater_or_equal.py
@@ -1,6 +1,6 @@
-average_mark = int(input("Enter the average mark"))
+average_mark = int(input("Enter the average mark :"))
 
 if (average_mark >= 50):
-    print(f"The average mark is equal and greater")
+    print(f"The average mark is pass")
 else:
-    print(f"The average mark is not greater and equal")
+    print(f"The average mark is fail")
