8913ef05a6af6707cc3336846efa6902e40de29e
AMB-007 <arjunmbabu777@gmail.com>
2026-06-22 09:21:09 +0530

    Added new Python programs

--- swap_two_no.py
+++ swap_two_no.py
@@ -1,10 +1,12 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
 
-print(f"Before Swapping: num1 = {num1}, num2 = {num2}")
 
-num1 = num1 + num2
-num2 = num1 - num2
-num1 = num1 - num2
 
-print(f"After Swapping: num1 = {num1}, num2 = {num2}")
+num_1 = 10
+num_2 = 20
+num_1 , num_2 = num_2 , num_1
+
+temp = num_2
+num_2 = num_2
+num_2 = num_1
+
+print(f"After Swapping: num1 = {num_1}, num2 = {num_2}")
