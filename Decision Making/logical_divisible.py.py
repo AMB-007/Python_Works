9f63d10ce896cbe2fc2a28d20e7be2f4871e647f
AMB-007 <arjunmbabu777@gmail.com>
2026-06-23 09:24:31 +0530

    Added new Python programs

---
+++ Decision Making/logical_divisible.py.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number :"))
+
+if num % 3 == 0 and num % 5 ==0:
+    print(f"The number is divisible by 3 and 5")
+else:
+    print(f"The number is not divisible by 3 and 5")
