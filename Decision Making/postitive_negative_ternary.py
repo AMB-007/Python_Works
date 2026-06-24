95bb2646f4fad246ea6816a20c3ccce58988380b
AMB-007 <arjunmbabu777@gmail.com>
2026-06-24 09:54:08 +0530

    Added new Python programs

---
+++ Decision Making/postitive_negative_ternary.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number"))
+
+
+print(f"its positive " if num > 0 else "its negative" )
+
+##################
+#divisible by 3 and 5
+
+
+print(f"its divisible by 3 and 5" if num %3 == 0 and num %5 == 0 else "its not divisible by 3 and 5")
---
+++ Decision Making/ternary_operator.py
@@ -0,0 +1,4 @@
+num = int(input("RNter a number to check its even or not :"))
+
+print(f"its even" if num %2 == 0 else "its odd")
+reult = f"{num} is even" if num %2 == 0 else f"{num} is odd"
