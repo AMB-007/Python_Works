4724e42647ffabf53da2f23ebf91ee5759c846b2
AMB-007 <arjunmbabu777@gmail.com>
2026-07-06 10:38:45 +0530

    code updated

--- 06-07-26/string/first_repeating.py
+++ 06-07-26/string/first_repeating.py
@@ -1,3 +1,20 @@
 # wap to get the first repeating character from the string
 
+# text = "programming"
+
+# for i in text:
+#     if text.count(i) > 1:
+#         print(i)
+#         break 
+
+
+
+# wap to get the first two unique characters from the string
 text = "programming"
+count = 0
+for i in text:
+    if text.count(i) == 1:
+        print(i)
+        count += 1
+    if count == 2:
+        break
