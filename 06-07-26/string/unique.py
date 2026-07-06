acb487c84616d8b5a25822ef48b1a647476bab05
AMB-007 <arjunmbabu777@gmail.com>
2026-07-06 10:19:38 +0530

    code updated

--- 06-07-26/string/unique.py
+++ 06-07-26/string/unique.py
@@ -1,3 +1,11 @@
 # wap to get the unique characters from the string joined together
 
-text = "programming" #output = "poain"
+text = "programming"  # output = "programin"
+new = ""
+count = 0
+for i in text:
+    if text.count(i) == 1:
+        new += i
+        count += 1
+print(new)
+print(count)
