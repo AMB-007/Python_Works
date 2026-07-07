62b9e8e63b527636ca6cd342441febd882406803
AMB-007 <arjunmbabu777@gmail.com>
2026-07-07 09:55:21 +0530

    code updated

--- 07-07-26/Patterns/task01.py
+++ 07-07-26/Patterns/task01.py
@@ -2,6 +2,8 @@
 """
 for i in range(1,6):
     print("*",end="")
+
+
 """
 
 
@@ -12,15 +14,43 @@ for i in range(1,6):
 for i in range(1,6):
     print("*")
 
+
+Output
+*
+*
+*
+*
+*
+
 """
 
-
+"""
 for i in range(1,6):
     for i in range(1,6):
         print(i,end=" ")
     print()
 
 
+output
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+"""
 
+
+"""
+for i in range(1,4):
+    for i in range(1,6):
+        print("*",end="")
+    print()
+
+
+output
+*****
+*****
+*****
+"""
 
 
