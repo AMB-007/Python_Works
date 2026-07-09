dbefa885603c74dc2bc261aa84c528821b568dd9
AMB-007 <arjunmbabu777@gmail.com>
2026-07-09 09:35:57 +0530

    code updated

---
+++ 09-07-26/Functions/intro.py
@@ -0,0 +1,22 @@
+"""
+
+A block of code that performs a specific task. it can accept input and process it and return a specific result
+
+
+print()
+input()
+len()
+
+Advantages 
+===========
+code resuability
+===========
+
+write the code one and use it multiple times without rewritting it
+
+
+def functionname()
+    statement       #function body
+
+functionname()         #function caller
+"""
