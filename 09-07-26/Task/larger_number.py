f7f7407d17f69f40f48788049177167bc8e24a41
AMB-007 <arjunmbabu777@gmail.com>
2026-07-09 19:04:21 +0530

    Added new Python program

---
+++ 09-07-26/Task/larger_number.py
@@ -0,0 +1,6 @@
+def create_largest(a,b):
+    if a > b:
+        print(a)
+    else:
+        print(b)
+create_largest(10,20)
---
+++ 09-07-26/Task/voting.py
@@ -0,0 +1,6 @@
+def vote(age):
+    if age >= 18:
+        print("eligible for vote")
+    else:
+        print("not eligible for voting")
+vote(17)
