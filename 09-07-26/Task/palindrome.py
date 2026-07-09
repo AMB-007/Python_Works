159967bd0532e7765ec522c8f64675bfe91ca18d
AMB-007 <arjunmbabu777@gmail.com>
2026-07-09 19:05:07 +0530

    Added new Python program

---
+++ 09-07-26/Task/palindrome.py
@@ -0,0 +1,16 @@
+def palindrome(word):
+    if word[::-1] == word:
+        print("palindrome")
+    else:
+        print("not palindrome")
+palindrome("malayalam")
+
+
+
+
+
+
+
+
+
+
---
+++ 09-07-26/Task/upper_case.py
@@ -0,0 +1,5 @@
+def upper_case(text):
+    for ch in text:
+        if ch.isupper():
+            print(ch,end=" ")
+upper_case("PyTHon")
