8d2ada72591aff79e5af4bb4e8df4e83f3ff896f
AMB-007 <arjunmbabu777@gmail.com>
2026-07-09 10:05:50 +0530

    code updated

--- 09-07-26/Functions/add_num.py
+++ 09-07-26/Functions/add_num.py
@@ -5,3 +5,14 @@ def add(a,b):
     sum = a + b
     print(sum)
 add(10,20)
+
+
+
+
+
+
+def add(a:int,b:int):
+# a,b >> parameters is a variable declared in the function definition that recevies the values when the function is called each times
+    sum = a + b
+    print(sum)
+add(10,20) # arguments :Actual values passed to the function during the function
---
+++ 09-07-26/Functions/is.prime.py
@@ -0,0 +1,12 @@
+# define a function to check the given number is prime or not
+
+def is_prime(num_1):
+    
+    if num_1 % 2 != 0:
+        print("prime")
+    else:
+         print("not prime")
+is_prime(7)
+
+
+
