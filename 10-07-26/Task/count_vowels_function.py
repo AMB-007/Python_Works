46b23b7055460a154eb41785eb731cb97cd85f3f
AMB-007 <arjunmbabu777@gmail.com>
2026-07-13 18:31:15 +0530

    Added new Python program

---
+++ 10-07-26/Task/count_vowels_function.py
@@ -0,0 +1,14 @@
+def count_vowels(text):
+
+    count = 0
+
+    for ch in text.lower():
+        if ch in "aeiou":
+            count += 1
+
+    return count
+
+
+text = input("Enter a string: ")
+
+print("Vowels =", count_vowels(text))
