cc9420519a9d6892878d69cbd3beab95e7168ca1
AMB-007 <arjunmbabu777@gmail.com>
2026-07-13 18:32:02 +0530

    Added new Python program

---
+++ 10-07-26/Task/sum_of_digits.py
@@ -0,0 +1,15 @@
+def sum_of_digits(num):
+
+    total = 0
+
+    while num > 0:
+        digit = num % 10
+        total += digit
+        num = num // 10
+
+    return total
+
+
+num = int(input("Enter a number: "))
+
+print("Sum of Digits =", sum_of_digits(num))
