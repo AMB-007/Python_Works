63ddd029c723032b6ebc41e86fae509d726aaa27
AMB-007 <arjunmbabu777@gmail.com>
2026-07-13 09:41:54 +0530

    code updated

---
+++ 13-07-26/anagram.py
@@ -0,0 +1,12 @@
+def is_anagram(text_1,text_2):
+    if len(text_1) == len(text_2):
+        for i in text_1:
+            if text_1.count(i) != text_2.count(i):
+                print("not anagram")
+                break
+        else:
+            print("anagram")
+
+    else:
+        print("not anagram")
+is_anagram("silnt","listen")
