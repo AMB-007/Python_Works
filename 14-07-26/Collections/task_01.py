7751298e6cf151a03cb7e0711b59cae7415d06d8
AMB-007 <arjunmbabu777@gmail.com>
2026-07-14 10:41:42 +0530

    Added new Python program

--- 14-07-26/Collections/task_01.py
+++ 14-07-26/Collections/task_01.py
@@ -1,5 +1,14 @@
-numbers = []
-n = int(input("Enter N "))
-for i in range(1,n+1):
-    numbers.append(i)
+# numbers = []
+# n = int(input("Enter N "))
+# for i in range(1,n+1):
+#     numbers.append(i)
+# print(numbers)
+
+
+numbers = [1,2,3,4,5]
+for i in range(3):
+    numbers.insert(0,numbers.pop())
 print(numbers)
+
+
+
