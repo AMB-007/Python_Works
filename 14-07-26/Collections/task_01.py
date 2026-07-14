c087727eb236a6a186f8a47d31f123b2a63901d3
AMB-007 <arjunmbabu777@gmail.com>
2026-07-14 20:36:42 +0530

    Added new Python program

--- 14-07-26/Collections/task_01.py
+++ 14-07-26/Collections/task_01.py
@@ -5,10 +5,24 @@
 # print(numbers)
 
 
-numbers = [1,2,3,4,5]
-for i in range(3):
-    numbers.insert(0,numbers.pop())
-print(numbers)
+# numbers = [1,2,3,4,5]
+# for i in range(3):
+#     numbers.insert(0,numbers.pop())
+# print(numbers)
 
 
+# numbers = [1,2,3,4,5]
+# count = 0
+# for i in numbers:
+#     count += 1
+# print(i)
+
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+total = 0
+for i in numbers:
+    if i %2 != 0:
+        total += i
+print(total)
 
