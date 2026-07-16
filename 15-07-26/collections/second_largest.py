500c081308b94102187f503468c2a087347ae3d6
AMB-007 <arjunmbabu777@gmail.com>
2026-07-16 09:54:23 +0530

    code updated

--- 15-07-26/collections/second_largest.py
+++ 15-07-26/collections/second_largest.py
@@ -2,12 +2,18 @@
 
 numbers = [4, 11, 5]
 
-largest = numbers[0]
-second_largest = numbers[1]
+if numbers[0] < numbers[1]:
+
+    largest = numbers[1]
+    second_largest = numbers[0]
+else:
+    largest = numbers[0]
+    second_largest = numbers[1]
 for i in numbers:
     if i > largest and i > second_largest:
         second_largest = largest
         largest = i
     elif i > second_largest and i < largest:
         second_largest = i
+
 print(largest,second_largest)
