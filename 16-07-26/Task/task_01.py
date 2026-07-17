3e270f0d86e05687224c4b760a7a413be349bef7
AMB-007 <arjunmbabu777@gmail.com>
2026-07-17 08:59:39 +0530

    Added new Python program

---
+++ 16-07-26/Task/task_01.py
@@ -0,0 +1,15 @@
+numbers = [1,2,3,4,5,6,7]
+target =  [9]    #[2,3,4]
+
+# define a function which accepts the list and return the three numbers sum
+# which equals to the target
+
+numbers = [3,4,5,1]
+target =  [7]#[3,4]
+
+# define a function which accepts the list and return the two numbers sum
+# which equals to the target
+
+def twonumbersum(elements):
+    pass
+twonumbersum(numbers)
